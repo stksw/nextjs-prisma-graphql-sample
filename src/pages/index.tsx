7c6dce5caa88d7a72728bc77d28a7610429e27ff
stksw <kakiage@gmail.com>
2022-10-01 17:53:05 +0900

    impl TodoList component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import type { NextPage } from 'next';
 import { signIn, signOut, useSession } from 'next-auth/react';
 
+import { TodoList } from '~/components/TodoList';
+
 const Home: NextPage = () => {
   const { data: session } = useSession();
 
   return (
     <>
       {session ? (
-        <>
+        <div className="container mx-auto p-4">
           Signed in as {session.user?.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
+          <button className="p-3 bg-gray-300" onClick={() => signOut()}>
+            Sign out
+          </button>
+          <TodoList />
+        </div>
       ) : (
         <>
           Not signed in <br />
